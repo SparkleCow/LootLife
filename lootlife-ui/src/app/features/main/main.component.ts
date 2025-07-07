4302b971b48089cbd3c414c5e8f2d604655922f9
SparkleCow <cjelcazador200@outlook.com>
2025-07-06 21:21:44 -0500

    feature main ui - creating landing page

---
+++ lootlife-ui/src/app/features/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { WindowComponent } from "../../shared/window/window.component";
+
+@Component({
+  selector: 'app-main',
+  imports: [WindowComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css'
+})
+export class MainComponent {
+
+}
