061b204d154beef42a14de88d00f5b7d9c83de9a
SparkleCow <cjelcazador200@outlook.com>
2025-07-16 19:51:31 -0500

    feature music - It linked with AWS S3 (Test) to reproduce music

--- lootlife-ui/src/app/app.component.ts
+++ lootlife-ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,6 +9,36 @@ import { Router, RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  @ViewChild('audioPlayer', { static: false }) audioPlayer!: ElementRef<HTMLAudioElement>;
+
+  /*Songs array in S3*/
+  songs = [
+    {
+      songUrl: "https://lootlife-bucket.s3.us-east-1.amazonaws.com/Nuclear+Daisies+-+Fingers+in+Your+Flower+Crown+(Official+Audio)+-+portrayal+of+guilt.mp3",
+      songName: "Nuclear Daisies - Finger in your flower crown"
+    },
+    {
+      songUrl: "https://lootlife-bucket.s3.us-east-1.amazonaws.com/Martin+Dupont+-+Inside+Out+(1987)+-+e0d9n0b5.mp3",
+      songName: "Martin Dupont - Inside Out"
+    },
+    {
+      songUrl: "https://lootlife-bucket.s3.us-east-1.amazonaws.com/%5B%ED%95%9C%EA%B8%80+%EA%B0%80%EC%82%AC%EB%B2%88%EC%97%AD%5D+Joey+Valence+%26+Brae+-+LIVE+RIGHT+-+Nomeans.mp3",
+      songName: "Joey Valence & Brae - LIVE RIGHT"
+    },
+    {
+      songUrl: "https://lootlife-bucket.s3.us-east-1.amazonaws.com/Machine+Girl+-+ATHOTH+A+GO+GO!!+%5BExtended%5D+-+Accursed+Alucard.mp3",
+      songName: "Machine Girl - ATHOTH A GO GO"
+    },
+    {
+      songUrl: "https://lootlife-bucket.s3.us-east-1.amazonaws.com/Machine+Girl+-+Motherfather+(Official+Video)+-+Machine+Girl.mp3",
+      songName: "Machine Girl - Motherfather"
+    },
+    {
+      songUrl: "https://lootlife-bucket.s3.us-east-1.amazonaws.com/ESPRIT+%E7%A9%BA%E6%83%B3%2C+George+Clanton+-+Warmpop+-+100+Electronica.mp3",
+      songName: " George Clanton - Warmpop"
+    }
+  ]
 
   constructor(private router:Router){}
 
@@ -26,5 +56,29 @@ export class AppComponent {
 
   redirectAtTask(){
     this.router.navigate(['/task']);
+  }
+
+  getRandomSongUrl(): string {
+    const randomIndex = Math.floor(Math.random() * this.songs.length);
+    return this.songs[randomIndex].songUrl;
+  }
+
+  playRandomSong(): void {
+    if (this.audioPlayer) {
+      const audio = this.audioPlayer.nativeElement;
+      const randomSongUrl = this.getRandomSongUrl();
+
+      // Pause actual song
+      audio.pause();
+
+      // Change song
+      audio.src = randomSongUrl;
+
+      // Load song
+      audio.load();
+      audio.play().catch(error => {
+        console.error('Error al reproducir el audio:', error);
+      });
+    }
   }
 }
