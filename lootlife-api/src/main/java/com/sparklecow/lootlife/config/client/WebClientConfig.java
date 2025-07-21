aa3ea740283440be3f89e803db83161346221853
SparkleCow <cjelcazador200@outlook.com>
2025-07-20 23:01:14 -0500

    feature ia - creating webclient

---
+++ lootlife-api/src/main/java/com/sparklecow/lootlife/config/client/WebClientConfig.java
@@ -0,0 +1,24 @@
+package com.sparklecow.lootlife.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${application.ia_agent.spark}")
+    private String agentKey;
+
+    @Bean
+    public WebClient openRouterWebClient() {
+        return WebClient.builder()
+                .baseUrl("https://openrouter.ai/api/v1/chat/completions")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + agentKey)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
